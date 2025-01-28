a9ced5b65ce097228f5a73594c70be8c61a54d51
unknown <rohitgurung086@gmail.com>
2025-01-28 15:30:38 +0530

    buttons updated

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../public/css/main.css'
+
+const Button = () => {
+  return (
+    <div>
+        <a className="cta-btn" href="index.html#about">Get Started</a>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/LiLink.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './../../public/css/main.css'
+
+const LiLink = () => {
+    return (
+        <>
+            <li><a href="#hero" className="active">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#team">Team</a></li>
+            <li className="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                    <li><a href="#">Dropdown 1</a></li>
+                    <li className="dropdown"><a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                            <li><a href="#">Deep Dropdown 1</a></li>
+                            <li><a href="#">Deep Dropdown 2</a></li>
+                            <li><a href="#">Deep Dropdown 3</a></li>
+                            <li><a href="#">Deep Dropdown 4</a></li>
+                            <li><a href="#">Deep Dropdown 5</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Dropdown 2</a></li>
+                    <li><a href="#">Dropdown 3</a></li>
+                    <li><a href="#">Dropdown 4</a></li>
+                </ul>
+            </li>
+            <li><a href="#contact">Contact</a></li>
+        </>
+    )
+}
+
+export default LiLink
